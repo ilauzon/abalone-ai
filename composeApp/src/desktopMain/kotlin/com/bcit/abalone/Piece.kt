b3246eb24710dff3565a7b4cc9a4495e939d0f0a
NICOLE_HSU <nicole.d.hsu@gmail.com>
2025-03-09 19:38:14 -0700

    Add conversion methods for pieces

--- composeApp/src/desktopMain/kotlin/com/bcit/abalone/Piece.kt
+++ composeApp/src/desktopMain/kotlin/com/bcit/abalone/Piece.kt
@@ -8,4 +8,24 @@ enum class Piece {
         White -> Black
         OffBoard -> OffBoard
     }
+
+    companion object {
+        fun convertPiece(colour: String): Piece {
+            val pieceColour = when (colour) {
+                "b" -> Black
+                "w" -> White
+                else -> Empty
+            }
+            return pieceColour
+        }
+    }
+
+    override fun toString(): String {
+        val piece = when (this) {
+            Black -> "b"
+            White -> "w"
+            else -> ""
+        }
+        return piece
+    }
 }
