d1ba3681164857208ccade01e734a48548711463
skcalvin <calvinhuango3o@gmail.com>
2025-03-27 14:57:21 -0700

    Changed closenessToCentre function in Isaac's heuristic to account for when the agent is playing black.

--- composeApp/src/desktopMain/kotlin/com/bcit/abalone/model/search/IsaacHeuristic.kt
+++ composeApp/src/desktopMain/kotlin/com/bcit/abalone/model/search/IsaacHeuristic.kt
@@ -28,7 +28,7 @@ class IsaacHeuristic: Heuristic {
      * @return a value when higher is best for Max, and when lower is best for Min.
      */
     override fun heuristic(state: StateRepresentation): Double {
-        val closeness = closenessToCentre(state.board) * CLOSENESS_WEIGHT
+        val closeness = closenessToCentre(state.board, state) * CLOSENESS_WEIGHT
         val adjacency = adjacency(state.board) * ADJACENCY_WEIGHT
 //        val moves = state.movesRemaining * MOVES_WEIGHT
         val sum = closeness + adjacency
@@ -54,7 +54,7 @@ class IsaacHeuristic: Heuristic {
          * @return the sum of white's distance to the centre, minus the sum of black's
          * distance to the centre.
          */
-        fun closenessToCentre(board: BoardState): Double {
+        fun closenessToCentre(board: BoardState, state: StateRepresentation): Double {
             val blackMarbles = board.cells.filter { it.value == Piece.Black }
             val whiteMarbles = board.cells.filter { it.value == Piece.White }
 
@@ -64,8 +64,13 @@ class IsaacHeuristic: Heuristic {
             blackSum += OFFBOARD_DISTANCE * (14 - blackMarbles.count())
             whiteSum += OFFBOARD_DISTANCE * (14 - whiteMarbles.count())
 
-            return (whiteSum - blackSum).toDouble()
+            return if (state.currentPlayer == Piece.White) {
+                (whiteSum - blackSum).toDouble()
+            } else {
+                (blackSum - whiteSum).toDouble()
+            }
         }
+
 
         /**
          * Computes the number of moves to move a marble from one coordinate to another,
